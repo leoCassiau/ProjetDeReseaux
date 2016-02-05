938590da3d402e81fd8fd2fa2b14e871ee8f8c4b
Jean-Christophe GUERIN <E15B505R@I004V2pc08.ensinfo.sciences.univ-nantes.prive>
2016-02-05 12:14:33 +0100

    Ajout de la struc socket dans shifumi

--- src/shifumi.c
+++ src/shifumi.c
@@ -1,23 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-using namespace std;
 
-enum coup {
+typedef enum coup {
     pierre,
     feuille,
     ciseaux
-};
+}coup;
 
-struct joueur {
-    string nom; // pseudo
+typedef struct  {
+    char nom[256]; // pseudo
     coup c;     // coup joué
     int score;  // score actuel du joueur
-};
+    int thread; //id du thread du client
+	bool isAlive; //
+	int rank; //Position du joueur dans la "ronde", premier arrivé premier servi, maj par les morts et deconnexions
+}joueur;
 
 bool bat(coup c1, coup c2) {
-    if( c1 == pierre && c2 == ciseaux ||
-        c1 == ciseaux && c2 == feuille ||
-        c1 == feuille && c2 == pierre) {
+    if( ((c1 == pierre) && (c2 == ciseaux)) ||
+        ((c1 == ciseaux) && (c2 == feuille)) ||
+        ((c1 == feuille) && (c2 == pierre))) {
         return true;
     }
     return false;
@@ -26,7 +31,7 @@ bool bat(coup c1, coup c2) {
 bool attaque(joueur j1, joueur j2) {
     if(bat(j1.c, j2.c)) {
         return true;
-    }
+    }else
     return false;
 }
 
@@ -36,5 +41,15 @@ bool defend(joueur j1, joueur j2) {
     }
     return false;
 }
+typedef enum Operation{
+	play,
+	report
+}Operation;
 
+
+struct socketMain{
+	Operation op;
+	joueur player[];
+	
+};
 
